bd850ea5f030b341e6d901260929b41f2749596b
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2025-08-30 23:48:00 +0530

    setting up appwrite

--- src/models/name.ts
+++ src/models/name.ts
@@ -1,1 +1,9 @@
-DASDA
+export const db = "main-stackflow";
+export const questionCollection = "questions";
+export const answerCollection = "answers";
+export const commentCollection = "comments";
+export const voteCollection = "votes";
+export const questionAttachmentBucket = "question-attachment";
+
+
+
