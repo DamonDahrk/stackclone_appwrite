dc0fc20da0156265ab0885191d5f4eec35185124
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2025-09-08 05:42:44 +0530

    vercel fix10

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,9 +1,15 @@
 const env = {
   appwrite: {
-    endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL),
-    projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
-    apikey: String(process.env.APPWRITE_API_KEY)
+    endpoint: process.env.NEXT_PUBLIC_APPWRITE_HOST_URL ?? "",
+    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID ?? "",
+    apikey: process.env.APPWRITE_API_KEY ?? "" // server-only
   }
+};
+
+if (!env.appwrite.endpoint || !env.appwrite.projectId) {
+  throw new Error(
+    "Appwrite endpoint or projectId not defined! Check your environment variables."
+  );
 }
 
-export default env
+export default env;
